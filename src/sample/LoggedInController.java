1f066caeb7b7ca6f4c9c719ef6568fcc8d6e27b0
sediiv <ivan.i.sedinkin@student.jyu.fi>
2025-01-18 23:47:00 +0200

    fxml style is complete + little code

--- src/sample/LoggedInController.java
+++ src/sample/LoggedInController.java
@@ -1,4 +1,40 @@
 package sample;
 
-public class LoggedInController {
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoggedInController implements Initializable {
+
+
+    @FXML
+    private Button button_logout;
+
+    @FXML
+    private Label label_welcome;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        button_logout.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+
+            }
+        });
+
+
+    }
+
+    public void setUserInformation(String username, String favChannel) {
+        label_welcome.setText("Welcome" + username + "!");
+        label_welcome.setText("Your favorite cahnnel is " + favChannel + "!");
+
+    }
 }
